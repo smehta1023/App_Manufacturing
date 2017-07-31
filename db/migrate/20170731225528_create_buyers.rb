bee3acd6ad5f5b37a1b52a53229abbb7fd9988ea
Jupiter <jupiterbaudot@gmail.com>
2017-07-31 16:33:04 -0700

    Add attributes to Buyer Model, modify Buy Migration

--- db/migrate/20170731225528_create_buyers.rb
+++ db/migrate/20170731225528_create_buyers.rb
@@ -1,25 +1,23 @@
 class CreateBuyers < ActiveRecord::Migration
   def change
     create_table :buyers do |t|
-      def change
-        create_table :buyers do |t|
-          t.references :user
-          t.string :first_name, null: false
-          t.string :last_name, null: false
-          t.string :company_name, null: false
-          t.string :linkedin
-          t.string :email, :limit => 35, null: false
-          t.string :account_type
-          t.string :account_status
-          t.string :address
-          t.integer :mobile_phone
-          t.integer :personal_phone
-          t.integer :business_phone
+        t.references :user
+        t.string :first_name, null: false
+        t.string :last_name, null: false
+        t.string :company_name, null: false
+        t.string :linkedin
+        t.string :email, :limit => 35, null: false
+        t.string :account_type
+        t.string :account_status
+        t.string :address
+        t.integer :mobile_phone
+        t.integer :personal_phone
+        t.integer :business_phone
 
-          t.timestamps null: false
-        end
-      end
+        t.timestamps null: false
 
     end
   end
 end
+
+
