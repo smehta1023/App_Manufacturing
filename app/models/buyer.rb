a19cd541715c1ac7ef42be86d8e5263f672828cd
Jupiter <jupiterbaudot@gmail.com>
2017-07-31 16:15:46 -0700

    Add linkedin and phone attributes to Buyer Model

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -4,12 +4,15 @@ class Buyer < ActiveRecord::Base
       t.references :user
       t.string :first_name
       t.string :last_name
+      t.string :company_name
+      t.string :linkedin
       t.string :email, :limit => 35
       t.string :account_type
       t.string :account_status
       t.string :address
-      t.integer :phone
-      t.references :user
+      t.integer :mobile_phone
+      t.integer :personal_phone
+      t.integer :business_phone
 
       t.timestamps
     end
