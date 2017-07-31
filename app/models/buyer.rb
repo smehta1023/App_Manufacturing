fed680bea8c9da7e5ba8ac993038addee18fdbc4
Jupiter <jupiterbaudot@gmail.com>
2017-07-31 16:23:08 -0700

    Move data to Buyer Migration, was in buyer model by mistake

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,20 +1,2 @@
 class Buyer < ActiveRecord::Base
-  def change
-    create_table :buyers do |t|
-      t.references :user
-      t.string :first_name
-      t.string :last_name
-      t.string :company_name
-      t.string :linkedin
-      t.string :email, :limit => 35
-      t.string :account_type
-      t.string :account_status
-      t.string :address
-      t.integer :mobile_phone
-      t.integer :personal_phone
-      t.integer :business_phone
-
-      t.timestamps
-    end
-  end
 end
